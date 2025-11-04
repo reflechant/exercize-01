1bfc717ce23abad4ab450300f2f4a280b1381c6f
Roman Gerasimov <reflechant@users.noreply.github.com>
2025-11-04 01:54:28 +0100

    add input processing

--- src/main.zig
+++ src/main.zig
@@ -10,8 +10,13 @@ pub fn main() !void {
     try glfw.init();
     defer glfw.terminate();
 
+    glfw.windowHint(glfw.WindowHint.context_version_major, 4);
+    glfw.windowHint(glfw.WindowHint.context_version_minor, 1);
+    glfw.windowHint(glfw.WindowHint.opengl_profile, glfw.OpenGLProfile.opengl_core_profile);
+    glfw.windowHint(glfw.WindowHint.opengl_forward_compat, true);
+
     const window = try glfw.createWindow(600, 600, "OpenGL Window", null);
-    defer glfw.destroyWindow(window);
+    defer window.destroy();
 
     glfw.makeContextCurrent(window);
 
@@ -22,19 +27,29 @@ pub fn main() !void {
     gl.makeProcTableCurrent(&procs);
     defer gl.makeProcTableCurrent(null);
 
+    gl.Viewport(0, 0, 600, 600);
+
+    _ = glfw.setFramebufferSizeCallback(window, framebuffer_size_callback);
+
     // setup your graphics context here
     while (!window.shouldClose()) {
-        glfw.pollEvents();
+        process_input(window);
 
-        // render your things here
-        // Use OpenGL functions
+        // rendering
         gl.ClearColor(0.0, 0.0, 0.0, 1.0);
         gl.Clear(gl.COLOR_BUFFER_BIT);
 
+        glfw.pollEvents();
         window.swapBuffers();
     }
 }
 
-// fn getProcAddress(_: ?*anyopaque, proc: [:0]const u8) ?*const anyopaque {
-//     return glfw.getProcAddress(proc);
-// }
+fn framebuffer_size_callback(_: *glfw.Window, width: c_int, height: c_int) callconv(.c) void {
+    gl.Viewport(0, 0, width, height);
+}
+
+fn process_input(window: *glfw.Window) void {
+    if (window.getKey(glfw.Key.escape) == glfw.Action.press) {
+        window.setShouldClose(true);
+    }
+}
